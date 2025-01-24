d0605f6ee693ed52c7d9b83257c2f96153035830
luismaDev <133109597+luismaa03@users.noreply.github.com>
2025-01-24 18:13:43 +0100

    Añadido Inicio sesion y Valoracion

---
+++ Chef-master/Chef/Valoracion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef
+{
+    class Valoracion
+    {
+
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int Estrellas { get; set; }
+        public string Comentario { get; set; }
+
+        public Valoracion() { }
+
+        public Valoracion(int usuarioId, int estrellas, string comentario)
+        {
+            if (estrellas < 1 || estrellas > 5)
+                throw new ArgumentOutOfRangeException(nameof(estrellas), "El número de estrellas debe estar entre 1 y 5.");
+
+            UsuarioId = usuarioId;
+            Estrellas = estrellas;
+            Comentario = comentario ?? string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"Usuario {UsuarioId}, Estrellas: {Estrellas}, Comentario: {Comentario}";
+        }
+    }
+}
