20314a35fbd8127e8e9a91915653aee5c01f0dfa
Alpame14 <167764600+Alpame14@users.noreply.github.com>
2025-02-11 21:10:33 +0100

    Ventana recetas sin estilo final pero casi

---
+++ Chef/Pasos.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace SeguirReceta
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Asignar eventos de MouseEnter y MouseLeave a los botones
+         
+        }
+
+        
+
+        
+
+       
+
+     
+    }
+}
