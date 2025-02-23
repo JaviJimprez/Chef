d95c1790cf1e8b588acd4e51a244bfd376b4d309
SGN <sergiogarcianavarrodam@gmail.com>
2025-02-23 21:05:36 +0100

    ventana pasos
    tiene que devolver a la listbox de crear receta la que se crea en el viewmodel comprobar tmb si en la ventana se puede usar el combo box para editar los pasos

--- Chef/clases/Paso.cs
+++ Chef/clases/Paso.cs
@@ -1,22 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Chef.clases
 {
-    public class Paso
+    public class Paso : INotifyPropertyChanged
     {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Descripcion { get; set; }
-        public double Cantidad { get; private set; }
+        private string _nombre;
+        private string _descripcion;
 
-        public Paso(string nombre, double cantidad)
+        public string Nombre
+        {
+            get => _nombre;
+            set { _nombre = value; OnPropertyChanged(); }
+        }
+
+        public string Descripcion
+        {
+            get => _descripcion;
+            set { _descripcion = value; OnPropertyChanged(); }
+        }
+
+        public Paso(string nombre, string descripcion)
         {
             Nombre = nombre;
-            Cantidad = cantidad;
+            Descripcion = descripcion;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
